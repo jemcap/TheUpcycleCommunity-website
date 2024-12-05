7164d697b61fc9b2085f0cd5745686289f87b2b4
Joshua. C <76528955+jemcap@users.noreply.github.com>
2024-12-05 18:35:28 +0000

    Feature: Implemented feature to validate user credentials

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,11 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import User from "../models/userModel.js";
 const userRoute = express.Router();
 
-// @desc Authenticate User
-// @route GET /api/users/auth
-// @access Public
-userRoute.get("/auth", async (req, res, next) => {
-  try {
-    return res.status(200).json({ message: "Authenticated" });
-  } catch (error) {
-    console.log(error.message);
-    next(error);
-  }
-});
-
-// @desc Authenticate User
-// @route GET /api/users/auth
+// @desc Register User
+// @route POST /api/users/register
 // @access Public
 userRoute.post("/register", async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -49,9 +36,45 @@ userRoute.post("/register", async (req, res, next) => {
   }
 });
 
-// userRoute.post("/", async (req, res, next) => {
-//   try {
-//   } catch (error) {}
-// });
+// @desc Login User
+// @route GET /api/users/login
+// @access Public
+userRoute.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password is required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await user.matchPasswords(password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "Invalid Email or Password. Please try again." });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
 
 export default userRoute;
